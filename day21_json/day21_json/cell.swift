1eb6c204d09c8b735d13fa5acafe9a50be7ecf3e
sara al zhrani <saraalzhrani@MacBook-Pro-alkhas-b-sara.local>
2021-10-24 21:49:40 +0300

    day21

---
+++ day21_json/day21_json/cell.swift
@@ -0,0 +1,27 @@
+//
+//  cell.swift
+//  day21_json
+//
+//  Created by sara al zhrani on 18/03/1443 AH.
+//
+
+import UIKit
+
+class cell : UITableViewCell{
+    
+    
+    
+    @IBOutlet weak var userId: UILabel!
+    
+    
+    @IBOutlet weak var id: UILabel!
+    
+    
+    
+    @IBOutlet weak var title: UILabel!
+    
+    @IBOutlet weak var body: UILabel!
+    
+    
+}
+
