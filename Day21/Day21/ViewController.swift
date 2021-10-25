a045ec1c60ba16f67dd03b3f5e5c1ad3ce4353dd
dmdm <deemamoh15@hotmail.com>
2021-10-25 14:20:31 +0300

    done

--- Day21/Day21/ViewController.swift
+++ Day21/Day21/ViewController.swift
@@ -21,7 +21,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+        let p = d [indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! Cell
+  
+        cell.idLable.text = "\(p.id)"
+        cell.titleLable.text = p.title
+        cell.bodyLable.text = p.body
+        return cell
     }
     
     
@@ -30,16 +36,35 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        simpleGetUrlRequest()
+        
     }
     
     func simpleGetUrlRequest() {
 
-        let url = URL(string: "https://jsonplaceholder.typicode.com/posts/1")!
+        let url = URL(string: "https://jsonplaceholder.typicode.com/posts")!
         let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
             guard let data = data else { return }
             let posts = try? JSONDecoder().decode([Post].self, from: data)
             print(String(data: data, encoding: .utf8)!)
+            d = posts ?? []
+            
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
         }
         task.resume()
     }
+}
+
+class Cell: UITableViewCell{
+    
+    @IBOutlet weak var idLable: UILabel!
+    @IBOutlet weak var titleLable: UILabel!
+    @IBOutlet weak var bodyLable: UILabel!
+    
 }
