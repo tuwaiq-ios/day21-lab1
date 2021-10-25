382a27888f30f7cfb21e3c91a9161482f0f5ab2a
Sana Alshahrani <me-sa51@hotmail.com>
2021-10-25 09:30:49 +0300

    Sana Alshaharni

---
+++ Task21/Task21/Cell.swift
@@ -0,0 +1,16 @@
+//
+//  Cell.swift
+//  Task21
+//
+//  Created by Sana Alshahrani on 18/03/1443 AH.
+//
+
+import UIKit
+
+class Cell: UITableViewCell {
+    
+    @IBOutlet weak var Id: UILabel!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var body: UILabel!
+    
+}
